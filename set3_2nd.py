87c6c60c9505552b18b0ee4a68f888b1dfed0566
Ashraf Ali Ibn Usman <90544985+ashrafali01@users.noreply.github.com>
2023-10-18 23:11:38 +0530

    Add files via upload

---
+++ set3_2nd.py
@@ -0,0 +1,32 @@
+#lex_auth_012693795044450304151
+
+def calculate_bill_amount(gems_list, price_list, reqd_gems,reqd_quantity):
+    bill_amount=0
+    #Write your logic here
+    for count,gem  in enumerate(reqd_gems):
+        print(count)
+        if gem not in gems_list:
+            return -1
+        index = gems_list.index(gem)
+        bill_amount+=price_list[index]*reqd_quantity[count]
+
+
+    if bill_amount>30000:
+        bill_amount*=0.95
+
+    return bill_amount
+
+#List of gems available in the store
+gems_list=["Emerald","Ivory","Jasper","Ruby","Garnet"]
+
+#Price of gems available in the store. gems_list and price_list have one-to-one correspondence
+price_list=[1760,2119,1599,3920,3999]
+
+#List of gems required by the customer
+reqd_gems=["Ivory","Emerald","Garnet"]
+
+#Quantity of gems required by the customer. reqd_gems and reqd_quantity have one-to-one correspondence
+reqd_quantity=[3,10,12]
+
+bill_amount=calculate_bill_amount(gems_list, price_list, reqd_gems, reqd_quantity)
+print(bill_amount)
